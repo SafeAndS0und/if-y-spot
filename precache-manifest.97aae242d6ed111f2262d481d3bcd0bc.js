54c49740108143cdf19a16f31283e15f22e708d4
Nikodem <nikodem.lorenz@gmail.com>
2019-08-26 20:07:23 +0200

    Updates

---
+++ precache-manifest.97aae242d6ed111f2262d481d3bcd0bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41bebd89ee602caf1e59973448f67412",
+    "url": "/totallynotspotify/index.html"
+  },
+  {
+    "revision": "8eb731b3353e742f9230",
+    "url": "/totallynotspotify/static/css/main.b7c2053a.chunk.css"
+  },
+  {
+    "revision": "9c58367fff568398e1f3",
+    "url": "/totallynotspotify/static/js/2.58e00d7a.chunk.js"
+  },
+  {
+    "revision": "8eb731b3353e742f9230",
+    "url": "/totallynotspotify/static/js/main.a4ee819a.chunk.js"
+  },
+  {
+    "revision": "fa5979d36aa52c6dad1b",
+    "url": "/totallynotspotify/static/js/runtime~main.06e7f091.js"
+  }
+]);
